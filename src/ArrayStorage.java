07dcbc4e86320f8873494d7aed26f7c1c4603db3
ws_mega <ws_mega@live.ru>
2022-08-02 23:29:51 +0700

    hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,77 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        int storageSize = size();
+        int i = 0;
+        while (i < storageSize) {
+            storage[i] = null;
+            i++;
+        }
     }
 
     void save(Resume r) {
+        int storageSize = size();
+        int i = 0;
+        while (i < storageSize) {
+            i++;
+        }
+        storage[i] = r;
     }
 
     Resume get(String uuid) {
+        int storageSize = size();
+        int i = 0;
+        while (i < storageSize) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+            i++;
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int storageSize = size();
+        boolean resumeDeleted = false;
+        int i = 0;
+        while (i < storageSize) {
+            if (storage[i].uuid.equals(uuid)) {
+                resumeDeleted = true;
+                break;
+            }
+            i++;
+        }
+        if (resumeDeleted) {
+            while (i < storageSize) {
+                if (i + 1 == storageSize) {
+                    storage[i] = null;
+                } else {
+                    storage[i] = storage[i + 1];
+                }
+                i++;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int storageSize = size();
+        Resume[] storageForReturn = new Resume[storageSize];
+        int i = 0;
+        while (i < storageSize) {
+            storageForReturn[i] = storage[i];
+            i++;
+        }
+        return storageForReturn;
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        while (i < storage.length && storage[i] != null) {
+            i++;
+        }
+        return i;
     }
 }
